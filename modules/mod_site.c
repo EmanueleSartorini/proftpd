5f9e5684494ca353589444b5e51ddcd3f111b00a
EmanueleSartorini <emanuele.sartorini@cblelectronics.com>
2025-06-30 08:41:18 +0200

    wip

--- modules/mod_site.c
+++ modules/mod_site.c
@@ -376,7 +376,9 @@ MODRET site_status(cmd_rec *cmd) {
 
   uint32_t RD_status = (storage_status << 16) | partition_status;*/
 
-  if(dtd_word.word3 & 0x0F == 0x00){
+  pr_response_add(R_200, "0x%08x" dtd_word.word3 & 0xF);
+
+  if((dtd_word.word3 & 0x0F) == 0x00){
     //4 bits LSB. Refer to partition 1. The partition is present and mounted
     pr_response_add(R_200, "0x%04x,0x%08x", dtd_word.word1, (uint32_t)(dtd_word.word2 << 16) | dtd_word.word3);
   }else if((dtd_word.word3 & 0x0F) == 0x02){
